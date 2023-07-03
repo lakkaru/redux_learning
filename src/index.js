74a209e6e4e72edf14048bf3a2951dc825ade90f
Lakshman Karunasena <lakkaru@gmail.com>
2023-07-03 07:46:15 +0530

    feat- action creator added.

--- src/index.js
+++ src/index.js
@@ -8,30 +8,46 @@ import store from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(store.getState());
 
-store.subscribe(()=>{
+store.subscribe(() => {
   console.log(store.getState());
 });
-store.dispatch({
-  type: "create_todo",
-  payload: {
-    id: "01",
-    task: "Learn React",
-    dateTime: "2023-Jul-01 20:30",
-    desc: "Learn the concepts of react",
-    status: "notStarted",
-  },
-});
-store.dispatch({
-  type: "create_todo",
-  payload: {
-    id: "02",
-    task: "Learn ReactJs",
-    dateTime: "2023-Jul-01 20:30",
-    desc: "Learn the concepts of react",
-    status: "notStarted",
-  },
-});
+// store.dispatch({
+//   type: "create_todo",
+//   payload: {
+//     id: "01",
+//     task: "Learn React",
+//     dateTime: "2023-Jul-01 20:30",
+//     desc: "Learn the concepts of react",
+//     status: "notStarted",
+//   },
+// });
+
+store.dispatch(createAction('01', "Learn React", "2023-Jul-01 20:30","Learn the concepts of react"));
+// store.dispatch({
+//   type: "create_todo",
+//   payload: {
+//     id: "02",
+//     task: "Learn ReactJs",
+//     dateTime: "2023-Jul-01 20:30",
+//     desc: "Learn the concepts of react",
+//     status: "notStarted",
+//   },
+// });
+store.dispatch(createAction('02', "Learn ReactJs", "2023-Jul-01 20:30","Learn the concepts of react"));
+
 // console.log(store.getState());
+function createAction(id, task, dateTime, desc) {
+  return {
+    type: "create_todo",
+    payload: {
+      id,
+      task,
+      dateTime,
+      desc,
+      status: "notStarted",
+    },
+  };
+}
 
 root.render(
   <React.StrictMode>
